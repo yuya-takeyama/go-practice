2b4b782dfa302edb1fa4fa03b47e7dee5caabfb0
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-11-03 22:58:56 +0900

    Add timeout example

---
+++ mastering_concurrency_in_go/03_developing_a_concurrent_strategy/timeout/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ourCh := make(chan string, 1)
+
+	go func() {
+	}()
+
+	select {
+	case <- time.After(3 * time.Second):
+		fmt.Println("Enough's enough")
+		close(ourCh)
+	}
+}
