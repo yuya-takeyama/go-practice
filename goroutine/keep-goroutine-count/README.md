5c4ec5a13b506bf247f4712ea02061c3b39ea5ef
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-12-09 23:21:54 +0900

    Keeping goroutines count

---
+++ goroutine/keep-goroutine-count/README.md
@@ -0,0 +1,3 @@
+# Goroutine の数を保って並列実行
+
+[Goroutineの最大数を制御する方法](http://qiita.com/ruiu/items/d4ef16981b870a1b85af)を試す
---
+++ goroutine/keep-goroutine-count/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+	"sync"
+	"time"
+)
+
+// Goroutine の数を数えておく
+var gCnt = 0
+
+// Goroutine の ID 発番
+var gId = 1
+
+// ループを実行する回数の設定
+var loopCount = 500
+
+func main() {
+	// プロファイラを起動してブラウザで確認できるようにしておく
+	// http://localhost:6060/debug/pprof/goroutine?debug=2 とかで Goroutine の様子を確認できる
+	runtime.SetBlockProfileRate(1)
+	go func() {
+		http.ListenAndServe(":6060", nil)
+	}()
+
+	mutex := &sync.Mutex{}
+	wg := &sync.WaitGroup{}
+	wg.Add(loopCount)
+
+	c := make(chan bool, 10)
+	for i := 1; i <= loopCount; i++ {
+		c <- true
+		go func() {
+			mutex.Lock()
+			gCnt++
+			updateGoroutineCount()
+			myGid := gId
+			fmt.Printf("%d is began\n", gId)
+			gId++
+			mutex.Unlock()
+
+			time.Sleep(time.Duration(rand.Int31n(100)) * time.Millisecond)
+			defer func() {
+				mutex.Lock()
+				fmt.Printf("%d is finished\n", myGid)
+				gCnt--
+				updateGoroutineCount()
+				mutex.Unlock()
+
+				wg.Done()
+				<-c
+			}()
+		}()
+	}
+
+	wg.Wait()
+}
+
+func updateGoroutineCount() {
+	fmt.Printf("%d goroutines exist\n", gCnt)
+}
